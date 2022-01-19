b11dc26ca54ac7b09c9a0e15928de79108ede25f
Mitchanok <6003599@mborijnland.nl>
2022-01-19 17:48:41 +0100

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -29,48 +29,76 @@
     <body>
 <?php
 
-$errors = [];
-$errorMessage = '';
+if (isset($_POST['Email'])) {
 
-if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $email = $_POST['mail'];
-    $message = $_POST['text'];
+    
+    $email_to = "6003599@mborijnland.nl";
+    $email_subject = "Nieuwe opmerking";
 
-    if (empty($name)) {
-        $errors[] = 'vul een naam in';
+    function problem($error)
+    {
+        echo "sorry er zit een fout in de form ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
     }
 
-    if (empty($mail)) {
-        $errors[] = 'vul een E-mail in';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
+   
+    if (
+        !isset($_POST['Name']) 
+        !isset($_POST['mail']) 
+        !isset($_POST['text'])
+    ) {
+        problem('sorry er zit een fout in de form');
     }
 
-    if (empty($text)) {
-        $errors[] = 'Vul een text in';
+    $name = $_POST['Name']; 
+    $email = $_POST['mail']; 
+    $message = $_POST['Text']; 
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'Deze mail bestaat niet.<br>';
     }
 
+    $string_exp = "/^[A-Za-z .'-]+$/";
 
-    if (empty($errors)) {
-        $toEmail = '6003599@mborijnland.nl';
-        $emailSubject = 'New email from your contant form';
-        $headers = ['From' => $mail, 'Reply-To' => $mail, 'Content-type' => 'text/html; charset=iso-8859-1'];
-
-        $bodyParagraphs = ["Name: {$name}", "Email: {$mail}", "Message:", $text];
-        $body = join(PHP_EOL, $bodyParagraphs);
-
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: .html');
-        } else {
-            $errorMessage = 'Oops, iets ging mis probeer het later nog een keer';
-        }
-    } else {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'Dit is geen echte naam.<br>';
     }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'voer een echte opmerking in.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+    bedankt we nemen zo snel mogelijk contact met je op
+
+<?php
 }
-
-
 ?>
     </Body>
